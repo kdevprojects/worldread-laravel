4ee4ecbb071211920c70b1cb3f084cfe16095ea6
root <root@localhost.localdomain>
2021-03-26 00:43:46 +0000

    v0.1.1

--- resources/angular/src/environments/environment.prod.ts
+++ resources/angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost/api',
-  oauth_client_id: 5,
-  oauth_client_secret: 'JXHNr3VMgrbaP3769kom2rxi9RnLAqunYTzTkfdk',
+  apiUrl: 'http://46.41.139.133/api',
+  oauth_client_id: 3,
+  oauth_client_secret: 'HA6hLIxMMMUnJPea8K0NVdbfPcfeoXaR66HDuD2Q',
 };
